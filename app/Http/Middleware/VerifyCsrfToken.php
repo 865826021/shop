0a8701dc18df6afb135c27afe9511188e13d94f8
yuxiaoyang <865826021@qq.com>
2017-09-03 03:53:57 +0000

    update

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,6 +14,7 @@ class VerifyCsrfToken extends BaseVerifier
     protected $except = [
         'appIndex',
 		'categoryIndex',
-		'getProductByCid'
+		'getProductByCid',
+		'newGetProductByCid'
     ];
 }
